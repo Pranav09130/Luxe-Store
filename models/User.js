17afd740087b9ff66c734da5195512c5a033666a
Pranav09130 <pranavmedhe15@gmail.com>
2026-06-18 20:09:20 +0530

    Fix User model pre-save middleware

--- models/User.js
+++ models/User.js
@@ -14,10 +14,10 @@ const userSchema = new mongoose.Schema({
   avatar:    { type: String, default: null },
 }, { timestamps: true });
 
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = function(plain) {
